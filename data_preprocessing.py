cb231b304341b1acaf1638f388dbbad1408f9420
Erlend Veire <erlend.veire@gmail.com>
2023-10-05 11:23:21 +0200

    fixed path

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -6,14 +6,16 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Importing the dataset
-dataset = pd.read_csv('Datadrevet-gruppe-12/graduation_dataset.csv')
+dataset = pd.read_csv('graduation_dataset.csv')
 
 print(dataset.head(5))
 
-# Removing the last column
-dataset = dataset.iloc[:, :-1]
+print(dataset.describe())
 
-f = plt.figure(figsize=(15, 15))
-sns.heatmap(dataset.corr(),annot=False, cmap='RdBu',vmax=.3, center=0,
-            square=True, linewidths=.5, cbar_kws={"shrink": .5})
-plt.show()
+# Removing the last column
+# dataset = dataset.iloc[:, :-1]
+
+# f = plt.figure(figsize=(15, 15))
+# sns.heatmap(dataset.corr(),annot=False, cmap='RdBu',vmax=.3, center=0,
+#             square=True, linewidths=.5, cbar_kws={"shrink": .5})
+# plt.show()
