52814c037d627db83317347475cc7fb62c6e3ca3
Halvor Øverby <halvor.overby@gmail.com>
2023-10-06 13:37:21 +0200

    fixed annes fork for good??git add svm.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -5,56 +5,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-education_mapping = {
-    1: "Secondary Education - 12th Year of Schooling or Eq.",
-    2: "Higher Education - Bachelor's Degree",
-    3: "Higher Education - Degree",
-    4: "Higher Education - Master's",
-    5: "Higher Education - Doctorate",
-    6: "Frequency of Higher Education",
-    9: "12th Year of Schooling - Not Completed",
-    10: "11th Year of Schooling - Not Completed",
-    11: "7th Year (Old)",
-    12: "Other - 11th Year of Schooling",
-    14: "10th Year of Schooling",
-    18: "General commerce course",
-    19: "Basic Education 3rd Cycle (9th/10th/11th Year) or Equiv.",
-    22: "Technical-professional course",
-    26: "7th year of schooling",
-    27: "2nd cycle of the general high school course",
-    29: "9th Year of Schooling - Not Completed",
-    30: "8th year of schooling",
-    34: "Unknown",
-    35: "Can't read or write",
-    36: "Can read without having a 4th year of schooling",
-    37: "Basic education 1st cycle (4th/5th year) or equiv.",
-    38: "Basic Education 2nd Cycle (6th/7th/8th Year) or Equiv.",
-    39: "Technological specialization course",
-    40: "Higher education - degree (1st cycle)",
-    41: "Specialized higher studies course",
-    42: "Professional higher technical course",
-    43: "Higher Education - Master (2nd cycle)",
-    44: "Higher Education - Doctorate (3rd cycle)"
-}
-
 # Importing the dataset
-dataset = pd.read_csv('graduation_dataset.csv')
-
-# Translate Mother's qualification column using the education_mapping
-dataset["Mother's qualification"] = dataset["Mother's qualification"].map(education_mapping)
-
+dataset = pd.read_csv('Datadrevet-gruppe-12/graduation_dataset.csv')
 
 print(dataset.head(5))
 
-print(dataset.describe())
-
-sns.scatterplot(x="Mother's qualification", y="Age at enrollment", data=dataset, hue=dataset["Target"])
-plt.show()
-
 # Removing the last column
-# dataset = dataset.iloc[:, :-1]
+dataset = dataset.iloc[:, :-1]
 
-# f = plt.figure(figsize=(15, 15))
-# sns.heatmap(dataset.corr(),annot=False, cmap='RdBu',vmax=.3, center=0,
-#             square=True, linewidths=.5, cbar_kws={"shrink": .5})
-# plt.show()
+f = plt.figure(figsize=(15, 15))
+sns.heatmap(dataset.corr(),annot=False, cmap='RdBu',vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+plt.show()
---
+++ svm.py
@@ -0,0 +1,71 @@
+from matplotlib.colors import ListedColormap
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix, classification_report
+
+
+# Load the dataset
+data = pd.read_csv("graduation_dataset2TokyoDrift.csv")
+target_mapping = {
+     "Dropout": 1,
+     "Enrolled": 1,
+     "Graduate": 1
+}
+
+data.replace({"Target": target_mapping}, inplace=True)
+
+print(data.info())
+
+# Split the data into training and test sets
+training_set, test_set = train_test_split(data, test_size=0.2, random_state=1)
+
+# Extract features and labels
+column= 29
+X_train = training_set.iloc[:, 0:column].values
+Y_train = training_set.iloc[:, column].values
+X_test = test_set.iloc[:, 0:column].values
+Y_test = test_set.iloc[:, column].values
+
+# Encode the class labels
+le = LabelEncoder()
+Y_train = le.fit_transform(Y_train)
+
+# Train the SVM classifier
+classifier = SVC()
+classifier.fit(X_train, Y_train)
+
+# Predict on the test set
+Y_pred = classifier.predict(X_test)
+test_set["Predictions"] = Y_pred
+
+for row in Y_pred:
+    
+    if row != 1 and row != 0:
+    
+        print(row)
+    
+    
+print(classification_report(Y_test, Y_pred))
+# Calculate accuracy
+cm = confusion_matrix(list(Y_test), Y_pred)
+accuracy = float(np.diagonal(cm).sum()) / len(Y_test)
+print("\nAccuracy Of SVM For The Given Dataset: ", accuracy)
+
+# Visualization (2D decision boundary for binary classification)
+# plt.figure(figsize=(7, 7))
+# X_set, y_set = X_train, Y_train
+# X1, X2 = np.meshgrid(np.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
+#                      np.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
+# plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape), alpha=0.75,
+#              cmap=ListedColormap(('black', 'white')))
+# plt.xlim(X1.min(), X1.max())
+# plt.ylim(X2.min(), X2.max())
+# for i, j in enumerate(np.unique(y_set)):
+#     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c=ListedColormap(('red', 'orange'))(i), label=j)
+# plt.title('Student dataset')
+# plt.legend()
+# plt.show()
